697ab8f7d8958337286919a047f2cba090d240d3
Abdalkarim Shehab <abdo.r.shehab2@gmail.com>
2023-03-11 13:24:33 +0200

    last changes added successfully

--- erpnext_customize/erpnext_customize/doc_event/material_request_event.py
+++ erpnext_customize/erpnext_customize/doc_event/material_request_event.py
@@ -1,13 +1,14 @@
 import frappe
 
 def create_stock_entry(doc, method):
-    new_stock_entry = frappe.new_doc("Stock Entry")
-    new_stock_entry.stock_entry_type = doc.material_request_type
-    new_stock_entry.from_warehouse = doc.set_from_warehouse
-    new_stock_entry.to_warehouse = doc.set_warehouse
+    if doc.material_request_type == "Material Transfer":
+        new_stock_entry = frappe.new_doc("Stock Entry")
+        new_stock_entry.stock_entry_type = doc.material_request_type
+        new_stock_entry.from_warehouse = doc.set_from_warehouse
+        new_stock_entry.to_warehouse = doc.set_warehouse
 
-    for item in doc.items:
-        new_stock_entry.append("items", {"item_code": item.item_code, "qty": item.qty})
+        for item in doc.items:
+            new_stock_entry.append("items", {"item_code": item.item_code, "qty": item.qty})
 
-    new_stock_entry.insert()
-    new_stock_entry.submit()
+        new_stock_entry.insert()
+        new_stock_entry.submit()
